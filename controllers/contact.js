9ae42cd15b3836b5ede3cadb36d0beff359485f4
Kurt Wyckoff <kurtitservices@gmail.com>
2017-03-15 14:24:06 -0400

    made ContactController and started index html page

---
+++ controllers/contact.js
@@ -0,0 +1,17 @@
+const SERVER_URL = "https://young-thicket-66297.herokuapp.com";
+
+function ContactController ($scope, $http, $state) {
+  console.log("Welcome to Contact Controller");
+  $scope.contacts = [];
+
+  function init () {
+    $http.get(`${SERVER_URL}/contacts`).then(resp => {
+      $scope.contacts = resp.data;
+    });
+  }
+
+  init();
+
+  ContactController.$inject = ['$scope', '$http', '$state'];
+
+  export default ContactController;
